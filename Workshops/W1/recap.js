04472743d3752f104a1bafa312dc88e9bd18967f
adamflyers <ademucar@gmail.com>
2021-03-23 21:05:29 +0200

    JS Recap

---
+++ Workshops/W1/recap.js
@@ -0,0 +1,20 @@
+document.write("Hellı World 2")
+console.log("Hello World")
+alert("Selam")
+confirm("Are we good?")
+console.table("selam", "nasılsın")
+console.error("Hata")
+console.warn("Uyarı")
+console.info("bilginiz için")
+// var => let, const
+let a = 6
+a = 5
+const b = 56
+c = true
+console.log(b)
+
+console.log(typeof b)
+
+let x
+let y = null
+console.log(typeof y) 
