a8f0befc113ebc108c65fcd6995cf0fa1f0f3b0e
adamflyers <ademucar@gmail.com>
2021-03-23 22:29:56 +0200

    JS Recap

---
+++ Workshops/JS-CC-002-Reverse/reverse.js
@@ -0,0 +1,11 @@
+function reverse(userInput){
+    let reversed = ""
+    let leftIndex = 0;
+    let rightIndex
+
+    return
+}
+
+let userInput = prompt("Type a word: ")
+let reversedUserInput = user(userInput)
+console.log("Reversed String: ",  reversedUserInput)
