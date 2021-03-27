1fdfa464f93c99bc2deba62ddf980006e4c48271
adamflyers <ademucar@gmail.com>
2021-03-27 12:21:14 +0200

    JS

--- Lessons/functions.js
+++ Lessons/functions.js
@@ -1,1 +1,13 @@
+ let sum = 0;
+ function pascalSum1(n){
+     return (n * (n + 1))/2
+ }
  
+ function pascalSum2(n){
+     var sum = 0;
+     while (n>= 0) {
+         sum += n
+         n--;
+     }
+     return sum;
+ }
