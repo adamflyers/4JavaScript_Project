0865013983685abaf940e045dbfc96d1c5210c69
adamflyers <ademucar@gmail.com>
2021-03-28 23:20:06 +0300

    JS

--- Lessons/functions.js
+++ Lessons/functions.js
@@ -11,3 +11,30 @@
      }
      return sum;
  }
+
+ function sayHi(myName){
+     console.log("Hello! ${myName}");
+     myName = "John";
+     console.log(nyName);
+ }
+
+ letmyName = "Matthew";
+ sayHi(myName);
+ console.log(myName);
+
+ function sayHi(name) {
+	console.log(`Hello! ${name}`);
+}
+sayHi('Matthew');
+sayHi(); // what will the output be
+
+function sayHi2(name, company) {
+    console.log(`Hello! ${name} of ${company}`);
+    company = 'Apple';
+    console.log(`Hello! ${name} of ${company}`);  
+}
+let name = 'Emir';
+let company = 'Clarusway';
+sayHi2(name, company);
+console.log(company);
+sayHi2('Matthew'); // what will the output be
