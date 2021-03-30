e5288826054fbbb9777514d14a0bd4de587e2dc5
adamflyers <ademucar@gmail.com>
2021-03-30 14:56:29 +0300

    JS_String

---
+++ Lessons/strings.js
@@ -0,0 +1,24 @@
+let data;
+
+//number --> String
+data = 20;
+data = String(20);
+data = String(5 + 5);
+
+//Bool --> String
+data = false;
+data = String(false)
+
+//Dates --> String
+data = String (new Date());
+
+//Array --> String
+data = String([0, 1, 2, 3]);
+
+data = (5).toString();
+data = true.toString();
+
+// Output
+console.log(data);
+console.log(typeof data);
+console.log(data.length);
