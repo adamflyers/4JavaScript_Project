a85d415bc643cce9d6de02eafa57b3572d55b931
adamflyers <ademucar@gmail.com>
2021-04-04 20:01:54 +0300

    JS_Maths

--- Lessons/math_operations.js
+++ Lessons/math_operations.js
@@ -1,8 +1,27 @@
 // JavaScript Math Operations
+// (+, -, /, *, %, ++, --)
 
 const sayi1 = 100;
 const sayi2 = 25;
 
 let sonuc;
 
-sonuc 
+sonuc1 = sayi1 + sayi2;
+sonuc2 = sayi1 - sayi2;
+sonuc3 = sayi1 * sayi2;
+sonuc4 = sayi1 / sayi2;
+sonuc5 = sayi1 % sayi2;
+sonuc6 = sayi1 ++;
+sonuc7 = ++sayi1;
+sonuc8 = sayi1 --;
+sonuc9 = --sayi1;
+
+console.log(sonuc1);
+console.log(sonuc2);
+console.log(sonuc3);
+console.log(sonuc4);
+console.log(sonuc5);
+console.log(sonuc6);
+console.log(sonuc7);
+console.log(sonuc8);
+console.log(sonuc9);
