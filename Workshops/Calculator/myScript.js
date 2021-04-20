f6e1194dc26b0cbd21c5340569b010943590de8a
adamflyers <ademucar@gmail.com>
2021-04-20 15:09:10 +0300

    JS

--- Workshops/Calculator/myScript.js
+++ Workshops/Calculator/myScript.js
@@ -1,3 +1,16 @@
-const num_1 = document.getElementById("inp_1").value;
 
-console.log(num_1);
+document.getElementById("btn_1").addEventListener("click", sumNumbers);
+
+
+function sumNumbers() {
+    const num_1 = document.getElementById("inp_1").value;
+    const num_2 = document.getElementById("inp_2").value;
+
+
+    if(isNaN(num_1) || isNaN(num_2))
+        return alert("Not a number")
+        
+    const result = parseInt(num_1) + parseInt(num_2);
+
+    document.getElementById("res").innerHTML = result;
+}
