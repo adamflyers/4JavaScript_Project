074c16f4161b7319cdd90bb2ca8d1457a6caacc3
adamflyers <ademucar@gmail.com>
2021-06-04 01:19:31 +0300

    JS

--- Recap_1/script.js
+++ Recap_1/script.js
@@ -7,12 +7,16 @@ const randomColorButton = document.querySelector("#randomColorButton");
 addColorButton.addEventListener("click", addColor);
 randomColorButton.addEventListener("click", changeColors);
 
+// 1) Arraydan rastgele element(renk) seçmek
+
 function changeColors() {
     const randomIndex = Math.floor(Math.random()*colors.length);
 
     document.querySelector("#colorName").textContent = colors[randomIndex];
     document.querySelector("body").style.backgroundColor = colors[randomIndex]
 } 
+
+// 2) Kullanıcının girdiği string'i renk listesine eklemek ve arkaplanı o renk yapmak
 
 function addColor() {
     const userInput = document.querySelector(".colorInput");
@@ -25,7 +29,7 @@ function addColor() {
     };
 
 
-    
+
     if(colors.indexOf(userInput.value) === -1) {
     document.querySelector("body").style.backgroundColor = userInput.value;
     colors.push(userInput.value);
