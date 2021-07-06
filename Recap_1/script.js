6674f68451fdbf8cb3a99e5100f141200a8fb20a
adamflyers <ademucar@gmail.com>
2021-07-06 23:16:55 +0300

    JS

--- Recap_1/script.js
+++ Recap_1/script.js
@@ -111,6 +111,7 @@ function addColor() {
         // 5d3) Farklı ise sıra dizinine dahil edilmesi ve ekranın temizlenmesi
 
         colors.push(colorObject);
+        
         userInput.value = "";
         userInput.focus();
         
