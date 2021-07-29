9cf45490d0b89f07504ad91c056083d0cb45d965
Adem Uçar <ademucar@gmail.com>
2021-07-29 23:32:32 +0200

    JS

--- Calculator/myScript.js
+++ Calculator/myScript.js
@@ -1,17 +1,1 @@
-
-document.getElementById("btn_1").addEventListener("click", sumNumbers);
-
-
-function sumNumbers() {
-    const num_1 = document.getElementById("inp_1").value;
-    const num_2 = document.getElementById("inp_2").value;
-
-
-    if(isNaN(num_1) || isNaN(num_2))
-        return alert("Not a number")
-        
-    const result = parseInt(num_1) + parseInt(num_2);
-
-
-    document.getElementById("res").innerHTML = result;
-}
+let dugme = document.getElementsByTagName("button");
