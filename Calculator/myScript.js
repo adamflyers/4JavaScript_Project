220fde444053194524ee312cf9280b9956f91eea
Adem Uçar <ademucar@gmail.com>
2021-07-30 09:59:20 +0200

    JS-Calc

--- Calculator/myScript.js
+++ Calculator/myScript.js
@@ -1,1 +1,14 @@
 let dugme = document.getElementsByTagName("button");
+
+let ekran = document.getElementsByClassName("ekran1")[0];
+
+for (var i = 0; i < 16; i++ ){
+    if( i != 12 && i != 14)
+    dugme[i].addEventListener("click", yazdir);
+};
+
+function yazdir(){
+    
+    ekran.value = ekran.value + this.value
+};
+
