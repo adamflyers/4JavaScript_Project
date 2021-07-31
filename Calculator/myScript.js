76dc49f2f8fb1634b2557e05af18df475a4d021e
Adem Uçar <ademucar@gmail.com>
2021-07-31 23:41:35 +0200

    Js

--- Calculator/myScript.js
+++ Calculator/myScript.js
@@ -1,28 +1,26 @@
 let dugme = document.getElementsByTagName("button");
-
 let ekran = document.getElementsByClassName("ekran1")[0];
-
-let hesapla = document.getElementById("hesapla");
-
+let yazdir = document.getElementById("yazdir");
 let temizle = document.getElementById("temizle");
 
- for (var i = 0; i < 16; i++ ){
-     if(i != 12 && i != 14)
+for (var i = 0; i < 16; i ++){
+    if(i != 12 && i != 14) {
     dugme[i].addEventListener("click", yazdir);
- };
+    }
+};
 
- function yazdir() {
-     ekran.value = ekran.value + this.value
- };
+function yazdir(){
+    ekran.value = ekran.value + this.value
+};
 
 hesapla.addEventListener("click", hesap);
 
- function hesap() {
-     ekran.value = eval(ekran.value);
- };
+function hesap(){
+    ekran.value = eval(ekran.value);
+};
 
- temizle.addEventListener("click", temiz);
+temizle.addEventListener("click", temiz);
 
- function temiz() {
-     ekran.value = " "
- };
+function temiz() {
+    ekran.value = " ";
+}
