027b2dd68cbd93f430ae9fede0b69dba870f4a6b
adamflyers <ademucar@gmail.com>
2021-09-19 23:04:48 +0200

    JS

--- Recap_2/index.js
+++ Recap_2/index.js
@@ -10,3 +10,5 @@ const color1 = document.getElementById("color2");
 const color1 = document.getElementById("color2");
 
 const color1 = document.getElementById("color2");
+
+const color1 = document.getElementById("color2");
