690345ecfe2cf8801972f96113a6650cf9a183db
Adem Uçar <ademucar@gmail.com>
2022-01-27 21:59:26 +0100

    JS

--- Clock/script.js
+++ Clock/script.js
@@ -3,4 +3,5 @@ let dugme = document.getElementsByTagName("button");
 let ekran = document.getElementsByClassName("ekran1")[0];
 
 let hesapla = document.getElementById("hesapla");
+
 let temizle = document.getElementById("temizle");
